e7671cf94a3a543252241b1b75bef4a1576f1baf
pangyongsheng <pangyongsheng@9fbank.com.cn>
2020-07-10 14:11:09 +0800

    打包

--- src/router/rwebgl.js
+++ src/router/rwebgl.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : your name
  * @Date         : 2020-04-10 15:26:24
- * @LastEditTime : 2020-07-03 18:22:19
+ * @LastEditTime : 2020-07-10 13:50:12
  * @LastEditors  : Please set LastEditors
  * @Description  : In User Settings Edit
  * @FilePath     : \vd\src\router\rd3.js
@@ -11,12 +11,12 @@ export default[
     {
         path: '/webgl',
         redirect: '/webgl/t1',
-        component: () => import(/* webpackChunkName: 'd3page' */ '@/views/About'),
+        component: () => import(/* webpackChunkName: 'wengl' */ '@/views/About'),
         children: dList.map(item => {
             return {
                 path: item.path,
                 name: item.path,
-                component: () => import(/* webpackChunkName: 'd3page' */ `@/views/webgl/${item.path}`)
+                component: () => import(/* webpackChunkName: 'wengl' */ `@/views/webgl/${item.path}`)
             }
         })
     },
